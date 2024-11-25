dfd6171e3c71b2effd3209f6b0ba43d14424e8d5
jeanbayiha24 <jean.bayiha@aims-senegal.org>
2024-11-25 19:22:47 +0000

    Create group1_data_app.py

---
+++ group1_data_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown("<h1 style='text-align: center; color: black;'>GROUP 1 DATA APP</h1>", unsafe_allow_html=True)
+
+st.markdown("""
+This app performs webscraping of data from dakar-auto over multiples pages. And we can also download scraped data from the app directly without scraping them.
+
+* **Python libraries:** base64, pandas, streamlit, requests, bs4
+* **Data source:** Expat-Dakar — Dakar-Auto.
+""")
