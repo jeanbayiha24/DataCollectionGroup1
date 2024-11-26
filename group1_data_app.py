6a1f90f41e344cea5abdc6a15ce543db4ee04537
jeanbayiha24 <jean.bayiha@aims-senegal.org>
2024-11-26 21:32:32 +0000

    Update group1_data_app.py

--- group1_data_app.py
+++ group1_data_app.py
@@ -2,6 +2,7 @@
 #from bs4 import BeautifulSoup as bs
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
+from webdriver_manager.chrome import ChromeDriverManager
 import streamlit as st
 import pandas as pd
 
