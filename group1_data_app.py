72fe6947be7341d5940c61ecab9a956a7897c38c
jeanbayiha24 <jean.bayiha@aims-senegal.org>
2024-11-27 02:28:58 +0000

    Update group1_data_app.py

--- group1_data_app.py
+++ group1_data_app.py
@@ -41,7 +41,7 @@ def scrape_all_bs(pages_nb, link):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
         
         url = f"{link}?page={page}"
-        res = get(url)
+        res = get(url, headers=headers)
         
         if res.status_code != 200:
             st.write(f"Erreur sur la page {page}: Code HTTP {res.status_code}")
