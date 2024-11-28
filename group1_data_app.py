412eb41cf010cf8e71c3a68880f48722d71f2843
jeanbayiha24 <jean.bayiha@aims-senegal.org>
2024-11-28 20:45:44 +0000

    Update group1_data_app.py

--- group1_data_app.py
+++ group1_data_app.py
@@ -85,7 +85,7 @@ def scrape_all_bs(pages_nb, link):
     'sec-ch-viewport-width': '792',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
         }
-        res = get(url, headers=request_headers)
+        res = get(url, headers=request_headers, timeout=30)
         if res.status_code != 200:
             st.write(f"Erreur sur la page {page}: Code HTTP {res.status_code}")
             continue
