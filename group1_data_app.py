4eb9e1e54ce434e89680e7dfd7a18cac0e5bd3b8
jeanbayiha24 <jean.bayiha@aims-senegal.org>
2024-11-28 14:19:26 +0000

    Update group1_data_app.py

--- group1_data_app.py
+++ group1_data_app.py
@@ -150,6 +150,7 @@ elif options == "Dashboard of the data":
     df_ordis = df_ordis.drop(['web-scraper-order','web-scraper-start-url'], axis = 1) #We drop the useless columns
     df_ordis['price'] = pd.to_numeric(df_ordis['price'].str.replace('F Cfa', '', regex=False).str.replace(' ', ''),  errors='coerce')#We clean the 'price' column
 
+    st.write(str(df_ordis.price.unique()))
     avg_prices_by_brand = df_ordis.groupby('brand')['price'].mean().dropna()
 
     plot1 = plt.figure(figsize=(11, 7))
