88129b4184f101e2d73e00871e059ed880b0c347
jeanbayiha24 <jean.bayiha@aims-senegal.org>
2024-11-28 16:07:38 +0000

    Update group1_data_app.py

--- group1_data_app.py
+++ group1_data_app.py
@@ -66,7 +66,7 @@ user_agents =[
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     ]
-
+proxy = "http://D4MgFT6C9WSP:DB06xRBK1v4s@superproxy.zenrows.com:1337"
 # Web scraping of Vehicles data on expat-dakar
 @st.cache_data
 def scrape_all_bs(pages_nb, link):
@@ -88,7 +88,7 @@ def scrape_all_bs(pages_nb, link):
     'sec-ch-ua-platform-version': '"10.0.0"',
     'sec-ch-viewport-width': '792',
         }
-        res = get(url, headers=request_headers, timeout=5)
+        res = get(url, headers=request_headers, proxies = http://D4MgFT6C9WSP:DB06xRBK1v4s@superproxy.zenrows.com:1337, timeout=5)
         if res.status_code != 200:
             st.write(f"Erreur sur la page {page}: Code HTTP {res.status_code}")
             continue
